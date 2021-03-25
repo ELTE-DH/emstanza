0e98615cb633c91e8b763f693fbc0ecdbdd5a103
levaid <levai753@gmail.com>
2021-03-25 15:57:23 +0100

    initial commit

---
+++ emstanzadep/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8, vim: expandtab:ts=4 -*-
+
+from .emstanzadep import EmStanzaDep
+from .version import __version__
+
+__all__ = ['EmStanzaDep', __version__]
